a752317ed67968b9ddc7503b846655b7db60ab20
Renan Martins da Silva <renanmartinsdasilva2007@gmail.com>
2026-05-15 20:32:12 -0300

    feat: Feito a transição de pagina do index para a previas

--- js/login.js
+++ js/login.js
@@ -1,6 +1,10 @@
 let loginForm = document.getElementById("loginForm");
 
 function verificarLogin() {
+
+    // Impede de recarregar a página
+    event.preventDefault();
+
     // Verificar se os botões tem conteudo e se estão certos
     let email = document.getElementById("email").value;
     let senha = document.getElementById("password").value;
@@ -10,8 +14,10 @@ function verificarLogin() {
         alert(`Bem vindo ${nome}, estamos te redirecionando para outra pagina!`)
 
         // Redirecionar para prox pagina
+        window.location.href = "previas.html";
     } else {
         alert("Login invalido, Algum dos dados inseridos estão incorretos");
     }
+    
 
 }
